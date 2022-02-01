ee6fb3d8049a58614b9cbb4e93984f7b0567cc2d
justusmarttila <marttilajustus2001@gmail.com>
2022-02-01 19:21:51 +0200

    tuplaus_valmis_01

--- grafiikka.py
+++ grafiikka.py
@@ -49,7 +49,7 @@ def napit(self):
     self.talteen_teksti = self.fontti.render("TALTEEN", True, (0, 0, 0))
 
 def voitto(self):
-    if self.tuplaus_valittu or self.tuplaus or self.tuplaus_jako_valmis
+    if self.tuplaus_valittu or self.tuplaus or self.tuplaus_jako_valmis:
         self.naytto.blit(self.voitto_maara_teksti, (self.leveys_ruutu-1100, self.korkeus_ruutu-190))
     if self.voitto or (self.tuplaus_valittu and self.tuplaus_voitto):
         tuplaus_voitot_talteen_napit(self)
